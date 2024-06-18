eb058ed481aff0de195b2644b448d507dd357077
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-06-19 02:36:14 +0300

    Add 1.6 and 1.7 solution

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good + neutral * 0 - bad) / total}</p>
+      <p>positive {(good / total) * 100}%</p>
+    </>
+  );
+};
+
+export default App;
